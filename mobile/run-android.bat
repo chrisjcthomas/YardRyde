c5b3789934f8a6e3a5957b6bebc5654083c84e56
zatguy79 <50187413+zatguy79@users.noreply.github.com>
2026-03-15 12:12:20 -0500

    Delete mobile directory

--- mobile/run-android.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "JAVA_HOME=C:\Users\cobek\.jdks\ms-21.0.10"
-set "ANDROID_HOME=C:\Users\cobek\AppData\Local\Android\Sdk"
-set "PATH=%JAVA_HOME%\bin;%ANDROID_HOME%\platform-tools;%PATH%"
-cd /d "C:\rtt"
-call npx react-native run-android --no-packager
